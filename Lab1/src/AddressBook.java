a776648056943e129916c54ddfea92ccb229b0a6
LenovoLaptop <mushmome1@gmail.ocm>
2020-09-25 11:02:13 -0400

    done

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class AddressBook {
+  public AddressBook()
+  {
+    BuddyInfo b1= new BuddyInfo("big show", "WWE arena", ":5198543212");
+    BuddyInfo b2= new BuddyInfo("showww", "arena alianz", ":5191222213");
+    BuddyInfo b3= new BuddyInfo("moe", "father arena", ":2264481865");
+
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("AddressBook");
+  }
+}
